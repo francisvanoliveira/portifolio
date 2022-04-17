4e3c7c7b1b1e0e928ceb58ab543884bc28ceaab8
Francisvan Oliveira <francisvan.oliveira@outlook.com>
2022-04-17 00:29:05 -0400

    📦 Versão 2.0
    Projeto reformulado por completo

---
+++ js/script.js
@@ -0,0 +1,60 @@
+let navbarDiv = document.querySelector('.navbar');
+window.addEventListener('scroll', () => {
+    if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40){
+        navbarDiv.classList.add('navbar-cng');
+    } else {
+        navbarDiv.classList.remove('navbar-cng');
+    }
+});
+
+
+const navbarCollapseDiv = document.getElementById('navbar-collapse');
+const navbarShowBtn = document.getElementById('navbar-show-btn');
+const navbarCloseBtn = document.getElementById('navbar-close-btn');
+// show navbar
+navbarShowBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.add('navbar-collapse-rmw');
+});
+
+// hide side bar
+navbarCloseBtn.addEventListener('click', () => {
+    navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target.id != "navbar-collapse" && e.target.id != "navbar-show-btn" && e.target.parentElement.id != "navbar-show-btn"){
+        navbarCollapseDiv.classList.remove('navbar-collapse-rmw');
+    }
+});
+
+// stop transition and animatino during window resizing
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+});
+
+//animação para o título da home
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+  }
+
+  // Se estiver tendo problemas com performance, utilize o FOR loop como abaixo no local do forEach
+  // function typeWriter(elemento) {
+  //   const textoArray = elemento.innerHTML.split('');
+  //   elemento.innerHTML = '';
+  //   for(let i = 0; i < textoArray.length; i++) {
+  //     setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
+  //   }
+  // }
+
+  const titulo = document.querySelector('h1');
+  typeWriter(titulo);
--- pages/404.php
+++
@@ -1,4 +0,0 @@
-<section class="center">
-    <a href="<?php echo INCLUDE_PATH; ?>">Voltar para o início</a>
-    <img src="<?php echo INCLUDE_PATH; ?>./img/Ops!.jpg">
-</section>
