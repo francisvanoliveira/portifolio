499c2e26d20a761e1623534361fe2bfb4ed6d081
Francisvan Oliveira <francisvan.oliveira@outlook.com>
2023-11-18 20:14:46 -0400

    🆙 Atualização
    Realizada atualização nos codigos

--- js/script.js
+++ js/script.js
@@ -47,14 +47,50 @@ function typeWriter(elemento) {
     });
   }
 
-  // Se estiver tendo problemas com performance, utilize o FOR loop como abaixo no local do forEach
-  // function typeWriter(elemento) {
-  //   const textoArray = elemento.innerHTML.split('');
-  //   elemento.innerHTML = '';
-  //   for(let i = 0; i < textoArray.length; i++) {
-  //     setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
-  //   }
-  // }
-
   const titulo = document.querySelector('h1');
   typeWriter(titulo);
+
+
+  // Pagina Galeria image modal
+  const allGalleryItem = document.querySelectorAll('.gallery-item');
+  const imgModalDiv = document.getElementById('img-modal-box');
+  const modalCloseBtn = document.getElementById('modal-close-btn');
+  const nextBtn = document.getElementById('next-btn');
+  const prevBtn = document.getElementById('prev-btn');
+  let imgIndex = 0;
+
+  allGalleryItem.forEach((galleryItem) => {
+      galleryItem.addEventListener('click', () => {
+          imgModalDiv.style.display = "block";
+          let imgSrc = galleryItem.querySelector('img').src;
+          imgIndex = parseInt(imgSrc.split("-")[1].substring(0, 1));
+          showImageContent(imgIndex);
+      })
+  });
+
+  // next click
+  nextBtn.addEventListener('click', () => {
+      imgIndex++;
+      if(imgIndex > allGalleryItem.length){
+          imgIndex = 1;
+      }
+      showImageContent(imgIndex);
+  });
+
+  // previous click
+  prevBtn.addEventListener('click', () => {
+      imgIndex--;
+      if(imgIndex <= 0){
+          imgIndex = allGalleryItem.length;
+      }
+      showImageContent(imgIndex);
+  });
+
+  function showImageContent(index){
+      // imgModalDiv.querySelector('#img-modal img').src = `images/gallery-${index}.jpg`;
+      imgModalDiv.querySelector('#img-modal img').src = `images/fotos/galeria_0${index}.jpg`;
+  }
+
+  modalCloseBtn.addEventListener('click', () => {
+      imgModalDiv.style.display = "none";
+  })
